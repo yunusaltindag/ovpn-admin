711bd120b937aeb7e0452620a7f2e28de56ff50e
xzxiaoshan <365384722@qq.com>
2025-03-23 15:33:30 +0800

    Comment the docker-compose plugin (#349)
    Signed-off-by: xzxiaoshan <365384722@qq.com>

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+# About 'docker compose' and 'docker-compose'
+# We are using Docker Compose in plugin mode with Docker. For more details, see: https://docs.docker.com/compose/install/linux/. If you need to use the standalone Docker Compose, you can modify the command `docker compose` to `docker-compose` accordingly.
 docker compose -p openvpn-master up -d --build
